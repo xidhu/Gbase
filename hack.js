ed5461fa3969cecbc72d1a0a7eb5a44106d8d9bd
Xidhu <64782756+xidhu@users.noreply.github.com>
2023-10-27 23:44:27 +0530

    Create hack.js

---
+++ hack.js
@@ -0,0 +1,5 @@
+// Generate a random number between 1 and 10
+const randomNumber = Math.floor(Math.random() * 10) + 1;
+
+// Print the random number to the console
+console.log("Random Number: " + randomNumber);
